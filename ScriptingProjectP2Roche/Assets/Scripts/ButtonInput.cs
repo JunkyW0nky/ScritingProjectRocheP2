fa1441eb42fe5e9aae8948ef6b828a3c55f147a4
JunkyW0nky <723469@student.dusd.net>
2025-03-12 09:42:10 -0700

    mousedown
    mousedown

--- ScriptingProjectP2Roche/Assets/Scripts/ButtonInput.cs
+++ ScriptingProjectP2Roche/Assets/Scripts/ButtonInput.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class ButtonInput : MonoBehaviour
 {
-    public ImageConversion graphic;
+ //   public Image graphic;
     public Sprite standard;
     public Sprite downgfx;
     public Sprite upgfx;
     public Sprite heldgfx;
-    public Text boolDisplay1;
-    public Text boolDisplay2;
-    public Text boolDisplay3;
+ //   public Text boolDisplay1;
+  //  public Text boolDisplay2;
+  //  public Text boolDisplay3;
 
     // Start is called before the first frame update
     void Start()
     {
-        graphic.sprite = standard;
+ //       graphic.sprite = standard;
     }
 
     // Update is called once per frame
@@ -28,23 +28,23 @@ public class ButtonInput : MonoBehaviour
 
         if(down)
         {
-            graphic.sprite = downgfx;
+    //        graphic.sprite = downgfx;
         }
         else if (held)
         {
-            graphic.sprite = heldgfx;
+    //        graphic.sprite = heldgfx;
         }
         else if (up)
         {
-            graphic.sprite = upgfx;
+      //      graphic.sprite = upgfx;
         }
         else
         {
-            graphic.sprite = standard;
+       //     graphic.sprite = standard;
         }
 
-        boolDisplay1.text = " " + down;
-        boolDisplay2.text = " " + held;
-        boolDisplay3.text = " " + held; ;
+     //   boolDisplay1.text = " " + down;
+      //  boolDisplay2.text = " " + held;
+      //  boolDisplay3.text = " " + held; ;
     }
 }
--- ScriptingProjectP2Roche/Assets/Scripts/KeyInput.cs
+++ ScriptingProjectP2Roche/Assets/Scripts/KeyInput.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 
 public class KeyInput : MonoBehaviour
 {
-    public Image graphic;
+//    public Image graphic;
     public Sprite standard;
     public Sprite downgfx;
     public Sprite upgfx;
     public Sprite heldgfx;
-    public Text boolDisplay1;
-    public Text boolDisplay2;
-    public Text boolDisplay3;
+ //   public Text boolDisplay1;
+   // public Text boolDisplay2;
+   // public Text boolDisplay3;
     // Start is called before the first frame update
     void Start()
     {
-        graphic.sprite = standard;
+   //     graphic.sprite = standard;
 
 }
 
@@ -29,24 +29,24 @@ public class KeyInput : MonoBehaviour
 
         if(down)
         {
-            graphic.sprite = downgfx;
+      //      graphic.sprite = downgfx;
 
         }
            else if (held)
         {
-            graphic.sprite = heldgfx;
+       //     graphic.sprite = heldgfx;
         }
         else if (up)
         {
-            graphic.sprite = upgfx;
+      //      graphic.sprite = upgfx;
         }
         else
         {
-            graphic.sprite = standard;
+      //      graphic.sprite = standard;
         }
 
-        boolDisplay1.text = " " + down;
-        boolDisplay2.text = " " + held;
-        boolDisplay3.text = " " + held;
+       // boolDisplay1.text = " " + down;
+       // boolDisplay2.text = " " + held;
+       // boolDisplay3.text = " " + held;
     }
 }
